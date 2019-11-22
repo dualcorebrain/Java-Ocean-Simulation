a35b2a138e427957aeef5957f49b6a8e49abb9ab
dualcorebrain <prasadanurag336@gmail.com>
2019-11-22 01:08:04 +0000

    Lab 6 step 3f, stuck on adjacent location

--- Unit 5 Ocean V2 (lab 6)/Unit 5/Lab 5 tasks/src/seaSimulations/Sardine.java
+++ Unit 5 Ocean V2 (lab 6)/Unit 5/Lab 5 tasks/src/seaSimulations/Sardine.java
@@ -1,7 +1,27 @@
 package seaSimulations;
 
 public class Sardine extends Fish {
-    public void act(Field theField){}
+    private Field fieldObject;
+    private Location location;
+    private Sardine Sardine;
+    private Object Creature;
+
+/*    public  Sardine (){}
+
+    public Sardine(int depthOfSardine , int widthOfSardine){
+        fieldObject = new Field(depthOfSardine , widthOfSardine);
+    }*/
+/*    public void act(Field theField){
+        if(location.equals(Creature)){
+            theField.place(Sardine,location);
+            theField.clear();;
+        }
+    }*/
+
+    public void act(Field theField, Location currentLocation){
+        theField.freeAdjacentLocation(location);
+        }
+    }
 
 
 
--- Unit 5 Ocean V2 (lab 6)/Unit 5/Lab 5 tasks/src/seaSimulations/Simulator.java
+++ Unit 5 Ocean V2 (lab 6)/Unit 5/Lab 5 tasks/src/seaSimulations/Simulator.java
@@ -100,11 +100,10 @@ public class Simulator {
     }
 
     public void simulateOneStep(){
-        for(Creature creatureIterator : creatureArrayList ){
-            if (creatureIterator instanceof )
+        //for(Creature creatureIterator : creatureArrayList ){
+            //if (creatureIterator instanceof )
 
         }
-    }
 
     public void startingSimulation(){
         simulatorViewContructor.showStatus(1, field);
@@ -114,51 +113,36 @@ public class Simulator {
     public static void main(String[] args) {
         Simulator s = new Simulator(100,100);
     }
-
-
-
-/*    public static void main(String[] args){*/
-/*        Plankton plankton = new Plankton();*/
-/*        Sardine sardine = new Sardine();*/
-/*        Shark shark = new Shark();*/
-/*
-*/
-
-/*        SimulatorView simulatorview = new SimulatorView(50,50);*/
-/*        Field field = new Field(50,50);*/
-/*
-*/
-
-/*        simulatorview.setColor(Plankton.class, Color.red);*/
-/*        simulatorview.setColor(Sardine.class, Color.green);*/
-/*        simulatorview.setColor(Shark.class, Color.blue);*/
-/*
-*/
-
-/*
-*/
-
-/*        field.place(plankton, 10,10);*/
-/*        field.place(sardine, 20,20);*/
-/*        field.place(shark, 30,30);*/
-/*
-*/
-
-/*
-*/
-
-/*        simulatorview.showStatus(1,field);*/
-/*
-*/
-
-/*
-*/
-
-/*
-*/
-
-/*
-*/
-
     }
+
+
+
+
+
+/*
+    public static void main(String[] args){
+        Plankton plankton = new Plankton();
+        Sardine sardine = new Sardine();
+        Shark shark = new Shark();
+
+        SimulatorView simulatorview = new SimulatorView(50,50);
+        Field field = new Field(50,50);
+
+        simulatorview.setColor(Plankton.class, Color.red);
+        simulatorview.setColor(Sardine.class, Color.green);
+        simulatorview.setColor(Shark.class, Color.blue);
+
+
+        field.place(plankton, 10,10);
+        field.place(sardine, 20,20);
+        field.place(shark, 30,30);
+
+
+        simulatorview.showStatus(1,field);
+
+
+
+*/
+
+
 
